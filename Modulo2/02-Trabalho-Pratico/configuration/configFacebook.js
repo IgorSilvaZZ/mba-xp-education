698f78517bb469273aa55a9836c8d8bc006256fc
IgorSilvaZZ <igor06silva12@gmail.com>
2024-03-19 23:14:11 -0300

    Remove secret and clientSecret facebook OAuth

--- Modulo2/02-Trabalho-Pratico/configuration/configFacebook.js
+++ Modulo2/02-Trabalho-Pratico/configuration/configFacebook.js
@@ -1,5 +1,5 @@
 module.exports = {
-  clientID: "431472189453351", // your App ID
-  clientSecret: "4b6f7388d2b0ceb6887cf3b075ec1e57", // your App Secret
+  clientID: "<CLIENT_ID>", // your App ID
+  clientSecret: "<CLIENT_SECRET_ID>", // your App Secret
   callbackURL: "http://localhost:3000/auth/facebook/callback",
 };
