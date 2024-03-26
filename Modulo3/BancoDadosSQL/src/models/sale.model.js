241f3c3bfeb73be798f85d77b86db25aa4fd3cd5
IgorSilvaZZ <igor06silva12@gmail.com>
2024-03-25 21:19:04 -0300

    Add model Sale

---
+++ Modulo3/BancoDadosSQL/src/models/sale.model.js
@@ -0,0 +1,32 @@
+import { DOUBLE, INTEGER, DATE } from "sequelize";
+
+import { sequelizeConnect } from "../database/connection.js";
+
+import { Client } from "./client.model.js";
+import { Product } from "./product.model.js";
+
+const Sale = sequelizeConnect.define(
+  "Sales",
+  {
+    saleId: {
+      type: INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    value: {
+      type: DOUBLE,
+      allowNull: false,
+    },
+    date: {
+      type: DATE,
+      allowNull: false,
+    },
+  },
+  { underscored: false }
+);
+
+Sale.belongsTo(Client, { foreignKey: "clientId" });
+Sale.belongsTo(Product, { foreignKey: "productId" });
+
+export { Sale };
