eae0642501b34c4925c4deebeddb28da49cd16d2
IgorSilvaZZ <igor06silva12@gmail.com>
2024-05-30 18:15:53 -0300

    Fisinh async module

--- BootcampReactJS/Modulo1/Assincrono/Aula3.5/index.js
+++ BootcampReactJS/Modulo1/Assincrono/Aula3.5/index.js
@@ -1,11 +1,63 @@
-// EXEMPLO DE Iterator
+// EXEMPLO FUNÇÃO GERADORA
 
-const carModel = ["Onix", "T-Cross", "HB20", "Palio"];
+function getIdNormal(range) {
+  let i = 0;
 
-// FOR
-// for (let index = 0; index < carModel.length; index++) {
-//   console.log(carModel[index]);
-// }
+  while (i < range) {
+    i++;
+
+    return i;
+  }
+}
+
+function* getIdGenerator(range) {
+  let i = 0;
+
+  while (i < range) {
+    i++;
+
+    // Declaração de rendimento
+    yield i;
+  }
+}
+
+let it = getIdGenerator(3);
+
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+
+let it2 = getIdGenerator(3);
+
+for (const item of it2) {
+  console.log(item);
+}
+
+function* getUniqueID() {
+  let id = 0;
+
+  while (true) {
+    id++;
+
+    // Declaração de rendimento
+    yield "id-" + id;
+  }
+}
+const cars = {};
+const idCarsGenerator = getUniqueID();
+
+function addCar(car) {
+  const id = idCarsGenerator.next().value;
+
+  cars[id] = { id, car };
+}
+
+addCar("Palio");
+addCar("Fox");
+addCar("Mobi");
+
+console.log(cars);
 
 const carModelAll = {
   allModels: {
@@ -51,8 +103,25 @@ const carModelAll = {
       },
     };
   },
+  *carGenerator() {
+    const brands = Object.values(this.allModels);
+
+    let currentBrandIndex = 0;
+
+    while (currentBrandIndex < brands.length) {
+      yield* brands[currentBrandIndex];
+      currentBrandIndex++;
+    }
+  },
 };
 
-for (const car of carModelAll) {
+let itA = carModelAll.carGenerator();
+
+console.log(itA.next());
+
+for (const car of carModelAll.carGenerator()) {
   console.log(car);
 }
+
+itB = carModelAll.carGenerator();
+console.log([...itB]);
