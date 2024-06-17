a560bebb754b07bb230a91061909bc200903512f
IgorSilvaZZ <igor06silva12@gmail.com>
2024-06-17 12:50:26 -0300

    Finish challenge module 2

--- BootcampReactJS/Modulo2/react-elections/web/src/components/CandidateInfo.jsx
+++ BootcampReactJS/Modulo2/react-elections/web/src/components/CandidateInfo.jsx
@@ -10,15 +10,21 @@ export const CandidateInfo = ({
   percent,
   candidateElection,
 }) => {
+  const candidateImage = `../../public/${userNameCandidate}.png`;
+
+  const colorElected = candidateElection ? "text-green-500" : "text-yellow-500";
+
   return (
     <div className='w-full flex items-center justify-between h-20 rounded-xl shadow-lg bg-zinc-800 px-4'>
       <div className='w-1/3 h-full flex items-center gap-4'>
-        <img
-          width='80'
-          height='80'
-          src='https://img.icons8.com/3d-fluency/94/user-male--v1.png'
-          alt='user-male--v1'
-        />
+        <div className='flex w-32 h-12'>
+          <img
+            className='rounded-full'
+            src={candidateImage}
+            alt='user-male--v1'
+          />
+        </div>
+
         <div className='flex flex-col'>
           <p className='text-white text-lg'>{nameCandidate}</p>
           <span className='text-sm font-semibold text-gray-400 w-44'>
@@ -27,7 +33,7 @@ export const CandidateInfo = ({
         </div>
       </div>
       <span className='text-white text-sm'>{percent}%</span>
-      <span className='text-white w-32 text-center'>
+      <span className={`text-white w-32 text-center ${colorElected}`}>
         {candidateElection ? "Eleito" : "Não Eleito"}
       </span>
     </div>
