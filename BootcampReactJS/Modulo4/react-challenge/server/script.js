1dcf5aa51fceb0e5177e6a63fd44d13033a69f91
IgorSilvaZZ <igor06silva12@gmail.com>
2024-07-17 21:50:28 -0300

    Initial implements list in web

--- BootcampReactJS/Modulo4/react-challenge/server/script.js
+++ BootcampReactJS/Modulo4/react-challenge/server/script.js
@@ -9,6 +9,7 @@ const yearFiltered = data[year];
     # Total de gols sofridos (independente se é casa ou fora/soma de ambos)
     # Total de Vitorias (independente se é casa ou fora/soma de ambos)
     # Total de Derrotas (independente se é casa ou fora/soma de ambos)
+    # Total de Empates(independente se é casa ou fora/soma de ambos)
     # Saldo de gols (Gols Feitos - Gols Sofridos)
     # Total de Pontos do time (Vitorias + Empates)
 */
@@ -18,32 +19,54 @@ const matchesLastRound = yearFiltered[yearFiltered.length - 1].partidas;
 const visitors = [];
 const principal = [];
 
+/* typeTeamMatcher => visitante ou mandante */
+const getInfoTeamMatch = (teamMatcher, typeTeamMatcher) => {
+  const nameTeamMatcher = teamMatcher[typeTeamMatcher];
+
+  const propertyResultsTeamMatcher = `pontuacao_geral_${typeTeamMatcher}`;
+
+  const resultsTeamMatcher = teamMatcher[propertyResultsTeamMatcher];
+
+  const {
+    total_gols_marcados,
+    total_gols_sofridos,
+    total_pontos,
+    total_vitorias,
+    total_empates,
+    total_derrotas,
+  } = resultsTeamMatcher;
+
+  const balanceGoals = total_gols_marcados - total_gols_sofridos;
+
+  return {
+    name: nameTeamMatcher,
+    totalPoints: total_pontos,
+    totalVictories: total_vitorias,
+    totalDraw: total_empates,
+    totalDefeats: total_derrotas,
+    goalsScored: total_gols_marcados,
+    goalsConceded: total_gols_sofridos,
+    balanceGoals,
+  };
+};
+
 for (const matchItem of matchesLastRound) {
-  if (
-    !visitors.find((visitorItem) => visitorItem.name === matchItem.visitante)
-  ) {
-    visitors.push({
-      name: matchItem.visitante,
-      totalPoints: matchItem.pontuacao_geral_visitante.total_pontos,
-    });
+  const { visitante: nameVisitor, mandante: namePrincipal } = matchItem;
+
+  if (!visitors.find((visitorItem) => visitorItem.name === nameVisitor)) {
+    const visitorInfo = getInfoTeamMatch(matchItem, "visitante");
+
+    visitors.push(visitorInfo);
   }
 
   if (
-    !principal.find(
-      (principalItem) => principalItem.name === matchItem.mandante
-    )
+    !principal.find((principalItem) => principalItem.name === namePrincipal)
   ) {
-    principal.push({
-      name: matchItem.mandante,
-      totalPoints: matchItem.pontuacao_geral_mandante.total_pontos,
-    });
+    const principalInfo = getInfoTeamMatch(matchItem, "mandante");
+
+    principal.push(principalInfo);
   }
 }
-
-/* console.log("Visitantes: ");
-console.log(visitors);
-console.log("Mandantes: ");
-console.log(principal); */
 
 /* const allMatches = [visitors, principal].flat(); */
 const allMatches = [...visitors, ...principal];
--- BootcampReactJS/Modulo4/react-challenge/web/app/page.tsx
+++ BootcampReactJS/Modulo4/react-challenge/web/app/page.tsx
@@ -1,113 +1,223 @@
 import Image from "next/image";
 
+import santosImg from "./assets/santos.png";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <main className='flex flex-col items-center gap-10 h-[90%] mx-auto w-3/4 bg-backgroundMain rounded-sm'>
+        <div className='flex gap-5 items-center justify-center w-full mt-4'>
+          <span className='font-semibold text-sm'>Selecione o Ano: </span>
+          <select className='rounded-xl w-60 text-center p-2 outline-none font-bold border border-zinc-500'>
+            <option value='2013'>2013</option>
+            <option value='2014'>2014</option>
+            <option value='2014'>2015</option>
+            <option value='2014'>2016</option>
+          </select>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <div className='w-10/12 flex flex-col gap-6 my-3 mx-1 h-full rounded-lg'>
+          <div className='w-full flex flex-items justify-end font-bold border-b-[1px] border-zinc-400'>
+            <span className='w-10 text-center'>P</span>
+            <span className='w-10 text-center'>V</span>
+            <span className='w-10 text-center'>E</span>
+            <span className='w-10 text-center'>D</span>
+            <span className='w-10 text-center'>GP</span>
+            <span className='w-10 text-center'>GC</span>
+            <span className='w-10 text-center'>S</span>
+          </div>
+          <div className='flex flex-col gap-1 items-center w-full h-[500px] overflow-y-auto'>
+            <div className='w-full h-20 flex flex-items rounded-lg'>
+              <div className='flex items-center gap-5 w-28 px-3'>
+                <span className='rounded-full font-bold text-base text-gray-600'>
+                  1
+                </span>
+                <Image
+                  src={santosImg}
+                  width={45}
+                  height={45}
+                  alt='Club Image'
+                />
+              </div>
+              <div className='flex items-center px-5 w-[700px]'>
+                <span className='text-gray-700 font-semibold text-lg'>
+                  Santos
+                </span>
+              </div>
+              <div className='flex flex-1 items-center justify-center'>
+                <span className='w-10 text-center'>71</span>
+                <span className='w-10 text-center'>19</span>
+                <span className='w-10 text-center'>15</span>
+                <span className='w-10 text-center'>12</span>
+                <span className='w-10 text-center'>76</span>
+                <span className='w-10 text-center'>76</span>
+                <span className='w-10 text-center'>14</span>
+              </div>
+            </div>
+            <div className='w-full h-20 flex flex-items rounded-lg'>
+              <div className='flex items-center gap-5 w-28 px-3'>
+                <span className='rounded-full font-bold text-base text-gray-600'>
+                  1
+                </span>
+                <Image
+                  src={santosImg}
+                  width={45}
+                  height={45}
+                  alt='Club Image'
+                />
+              </div>
+              <div className='flex items-center px-5 w-[700px]'>
+                <span className='text-gray-700 font-semibold text-lg'>
+                  Santos
+                </span>
+              </div>
+              <div className='flex flex-1 items-center justify-center'>
+                <span className='w-10 text-center'>71</span>
+                <span className='w-10 text-center'>19</span>
+                <span className='w-10 text-center'>15</span>
+                <span className='w-10 text-center'>12</span>
+                <span className='w-10 text-center'>76</span>
+                <span className='w-10 text-center'>76</span>
+                <span className='w-10 text-center'>14</span>
+              </div>
+            </div>
+            <div className='w-full h-20 flex flex-items rounded-lg'>
+              <div className='flex items-center gap-5 w-28 px-3'>
+                <span className='rounded-full font-bold text-base text-gray-600'>
+                  1
+                </span>
+                <Image
+                  src={santosImg}
+                  width={45}
+                  height={45}
+                  alt='Club Image'
+                />
+              </div>
+              <div className='flex items-center px-5 w-[700px]'>
+                <span className='text-gray-700 font-semibold text-lg'>
+                  Santos
+                </span>
+              </div>
+              <div className='flex flex-1 items-center justify-center'>
+                <span className='w-10 text-center'>71</span>
+                <span className='w-10 text-center'>19</span>
+                <span className='w-10 text-center'>15</span>
+                <span className='w-10 text-center'>12</span>
+                <span className='w-10 text-center'>76</span>
+                <span className='w-10 text-center'>76</span>
+                <span className='w-10 text-center'>14</span>
+              </div>
+            </div>
+            <div className='w-full h-20 flex flex-items rounded-lg'>
+              <div className='flex items-center gap-5 w-28 px-3'>
+                <span className='rounded-full font-bold text-base text-gray-600'>
+                  1
+                </span>
+                <Image
+                  src={santosImg}
+                  width={45}
+                  height={45}
+                  alt='Club Image'
+                />
+              </div>
+              <div className='flex items-center px-5 w-[700px]'>
+                <span className='text-gray-700 font-semibold text-lg'>
+                  Santos
+                </span>
+              </div>
+              <div className='flex flex-1 items-center justify-center'>
+                <span className='w-10 text-center'>71</span>
+                <span className='w-10 text-center'>19</span>
+                <span className='w-10 text-center'>15</span>
+                <span className='w-10 text-center'>12</span>
+                <span className='w-10 text-center'>76</span>
+                <span className='w-10 text-center'>76</span>
+                <span className='w-10 text-center'>14</span>
+              </div>
+            </div>
+            <div className='w-full h-20 flex flex-items rounded-lg'>
+              <div className='flex items-center gap-5 w-28 px-3'>
+                <span className='rounded-full font-bold text-base text-gray-600'>
+                  1
+                </span>
+                <Image
+                  src={santosImg}
+                  width={45}
+                  height={45}
+                  alt='Club Image'
+                />
+              </div>
+              <div className='flex items-center px-5 w-[700px]'>
+                <span className='text-gray-700 font-semibold text-lg'>
+                  Santos
+                </span>
+              </div>
+              <div className='flex flex-1 items-center justify-center'>
+                <span className='w-10 text-center'>71</span>
+                <span className='w-10 text-center'>19</span>
+                <span className='w-10 text-center'>15</span>
+                <span className='w-10 text-center'>12</span>
+                <span className='w-10 text-center'>76</span>
+                <span className='w-10 text-center'>76</span>
+                <span className='w-10 text-center'>14</span>
+              </div>
+            </div>
+            <div className='w-full h-20 flex flex-items rounded-lg'>
+              <div className='flex items-center gap-5 w-28 px-3'>
+                <span className='rounded-full font-bold text-base text-gray-600'>
+                  1
+                </span>
+                <Image
+                  src={santosImg}
+                  width={45}
+                  height={45}
+                  alt='Club Image'
+                />
+              </div>
+              <div className='flex items-center px-5 w-[700px]'>
+                <span className='text-gray-700 font-semibold text-lg'>
+                  Santos
+                </span>
+              </div>
+              <div className='flex flex-1 items-center justify-center'>
+                <span className='w-10 text-center'>71</span>
+                <span className='w-10 text-center'>19</span>
+                <span className='w-10 text-center'>15</span>
+                <span className='w-10 text-center'>12</span>
+                <span className='w-10 text-center'>76</span>
+                <span className='w-10 text-center'>76</span>
+                <span className='w-10 text-center'>14</span>
+              </div>
+            </div>
+            <div className='w-full h-20 flex flex-items rounded-lg'>
+              <div className='flex items-center gap-5 w-28 px-3'>
+                <span className='rounded-full font-bold text-base text-gray-600'>
+                  1
+                </span>
+                <Image
+                  src={santosImg}
+                  width={45}
+                  height={45}
+                  alt='Club Image'
+                />
+              </div>
+              <div className='flex items-center px-5 w-[700px]'>
+                <span className='text-gray-700 font-semibold text-lg'>
+                  Santos
+                </span>
+              </div>
+              <div className='flex flex-1 items-center justify-center'>
+                <span className='w-10 text-center'>71</span>
+                <span className='w-10 text-center'>19</span>
+                <span className='w-10 text-center'>15</span>
+                <span className='w-10 text-center'>12</span>
+                <span className='w-10 text-center'>76</span>
+                <span className='w-10 text-center'>76</span>
+                <span className='w-10 text-center'>14</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
   );
 }
